2a6f01194e0610f1b1416fa554f647d59f20f0ae
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-09-04 12:28:15 +0200

    Update with some fixes for initial commit

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# craftbeerpi4-plugin-template
+# Craftbeerpi4 Actor Plugin that switched Actor off depending on GPIO input state
 
-# THIS IS AN ALPHA VERSION
+## THIS IS AN ALPHA VERSION
 
-## NOT TESTED YET
+- Functions:
+	- This Plugin can switch off an actor depending on a GPIO input state
+
+- Parameters:
+	- Actor: The Actor that has to be switched off
+	- GPIO: The GPIO that s used as input for a high / low signal
+	- GPIOstate: High or low. If set to high, the Actor will be siwtched off, when the GPIO turns from low to high. If set to low, it'll work from high to low
+	- Notification: If set to Yes, a notification will be sent, when the GPIO triggers the actor
+	
+- Installation:
+	- log in via ssh 
+	- clone from the GIT repo
+	- sudo pip install ./GPIODependentActor
+	- cbpi add cbpi4-GPIODependentActor
+	
+- Changelog:
+	- 04.09.21: Initial commit
+	
+- Known Problems:
+	- Not tested on my system as I have no GPIO inputs (User confirmation required) -> Therefore ALPHA version
+	- When GPIO switches off the actor, the state for this actor is not updated in the UI. The dependent actor state is updated. Further investigation required for fix
--- cbpi4-GPIODependentActor/__init__.py
+++ cbpi4-GPIODependentActor/__init__.py
@@ -12,10 +12,10 @@ import RPi.GPIO as GPIO
 
 logger = logging.getLogger(__name__)
 
-@parameters([Property.Actor(label="Actor",  description="Select the actor you would like to add a dependency to."),
-            Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]), 
+@parameters([Property.Actor(label="Actor",  description="Select the actor that will be switched off depending on the GPIO state."),
+            Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27], description="GPIO [BMC numbering] that will switch off the Actor"), 
             Property.Select(label="GPIOstate", options=["High", "Low"],description="High: Actor switches off on GPIO high; Low: Actor switches off on GPIO low"),
-            Property.Select(label="notification", options=["Yes", "No"], description="Will show notifications in case of errors if set to yes")])
+            Property.Select(label="notification", options=["Yes", "No"], description="Will show notification when GPIO switches actor off")])
 
 class GPIODependentActor(CBPiActor):
 
@@ -25,13 +25,17 @@ class GPIODependentActor(CBPiActor):
             logging.info(inputsignal)
             if (self.dependency_type == "High") and (GPIO.input(int(self.ActorDependency))):
                 logging.info("Break on high")
-                await self.cbpi.actor.off(self.base)
+                await self.off()
                 self.state = False
+                if self.notification == "Yes":
+                    self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched off as GPIO {} siwtchted to {}".format(self.name, self.ActorDependency, self.dependency_type), NotificationType.INFO)
                 break
             elif (self.dependency_type == "Low") and not (GPIO.input(int(self.ActorDependency))):
                 logging.info("Break on Low")
-                await self.cbpi.actor.off(self.base)
+                await self.off()
                 self.state = False
+                if self.notification == "Yes":
+                    self.cbpi.notify("GPIO Dependent Actor", "Actor {} switched off as GPIO {} siwtchted to {}".format(self.name, self.ActorDependency, self.dependency_type), NotificationType.INFO)
                 break
             if self.interrupt == True:
                 break
@@ -40,6 +44,10 @@ class GPIODependentActor(CBPiActor):
     def on_start(self):
         self.state = False
         self.base = self.props.get("Actor", None)
+        try:
+            self.name = (self.cbpi.actor.find_by_id(self.base).name)
+        except:
+            self.name = ""
         self.ActorDependency = self.props.get("GPIO", None)
         self.dependency_type = self.props.get("GPIOstate", "High")
         self.notification = self.props.get("notification", "Yes")
